ad97294408ae8f3457d7650d18b87b8f08f1a43a
Masakaz Ozaki <r2d28192@gmail.com>
2018-06-21 00:29:25 +0900

    Initial Commit

---
+++ goldenRatio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  goldenRatio
+//
+//  Created by Masakaz Ozaki on 2018/06/21.
+//  Copyright © 2018 Masakazu Ozaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
